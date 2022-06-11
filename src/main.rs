2e1612df5d9db9b82a0fece8e5806a949a8457e9
marvil <marvil08@gmail.com>
2022-06-11 11:59:56 +0300

    pir + timer stable

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,19 @@ extern crate panic_semihosting;
 
 use cortex_m_rt::entry;
 use rtt_target::{rprintln, rtt_init_print};
-use stm32g0xx_hal::{block, prelude::*, stm32};
+use stm32g0xx_hal::{ prelude::*, stm32};
 use stm32g0xx_hal::rcc::Config;
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     rprintln!("start");
+    //let cp = cortex_m::Peripherals::take().expect("cannot take core peripherals");
     let dp = stm32::Peripherals::take().expect("cannot take peripherals");
     let mut rcc = dp.RCC.freeze(Config::lsi());
     //let  delay = dp.TIM14.delay(&mut rcc);
 
-    let  timeout = 4000;
+    let  timeout = 5000;
     let mut timer = dp.TIM16.timer(&mut rcc);
     let gpioa = dp.GPIOA.split(&mut rcc);
     //let gpioc = dp.GPIOC.split(&mut rcc);
@@ -25,6 +26,9 @@ fn main() -> ! {
     let pir = gpioa.pa12.into_pull_down_input();
     //pir.listen(SignalEdge.Rising, );
     let mut preview_pir_status = false;
+    let mut timer_need_start = false;
+    let mut timer_started = false;
+    //timer.start(timeout.ms());
 
     //let mut opto_pin = gpioc.pc14.into_analog();
     loop {
@@ -34,22 +38,37 @@ fn main() -> ! {
             _ => unreachable!(),
         };
         if pir_status {
-            timer.reset();
-            led.set_high().unwrap();
-            preview_pir_status = true;
-            rprintln!("pir up");
-        } else {
-            if preview_pir_status {
-                rprintln!("pir down after up");
-                timer.start(timeout.ms());
-                rprintln!("start timer");
-                block!(timer.wait()).unwrap();
-                led.set_low().unwrap();
-                //timer.reset();
-                //timer.clear_irq();
-                rprintln!("timer down {}",timer.get_current());
+
+            if preview_pir_status == false {
+
+                rprintln!("pir up");
+                led.set_high().unwrap();
+                preview_pir_status = true;
+                timer_need_start = true;
             }
-            preview_pir_status = false;
+            if timer_started {
+                timer.reset();
+            }
+        } else if preview_pir_status{
+
+            if timer_need_start {
+                timer_started = true;
+                timer.start(timeout.ms());
+            }
+            timer_need_start = false;
+
+            rprintln!("timer down {}", timer.get_current());
+
+            if timer.wait().is_ok() {
+                // новый цикл таймера
+                rprintln!("timer is ok");
+                led.set_low().unwrap();
+
+                timer.reset();
+                timer.clear_irq();
+                preview_pir_status = false;
+            }
+
         }
 
 
