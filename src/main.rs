3fb1e415774ea8cd28a7856f22671c5d2760b2bb
marvil <marvil08@gmail.com>
2022-06-12 17:23:28 +0300

    adc

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,9 @@ extern crate panic_semihosting;
 
 use cortex_m_rt::entry;
 use rtt_target::{rprintln, rtt_init_print};
-use stm32g0xx_hal::{ prelude::*, stm32};
+use stm32g0xx_hal::{ prelude::*, stm32, analog::adc::{OversamplingRatio, Precision, SampleTime}};
 use stm32g0xx_hal::rcc::Config;
+
 
 #[entry]
 fn main() -> ! {
@@ -16,14 +17,26 @@ fn main() -> ! {
     //let cp = cortex_m::Peripherals::take().expect("cannot take core peripherals");
     let dp = stm32::Peripherals::take().expect("cannot take peripherals");
     let mut rcc = dp.RCC.freeze(Config::lsi());
-    //let  delay = dp.TIM14.delay(&mut rcc);
+    let mut adc = dp.ADC.constrain(&mut rcc);
+    let mut delay = dp.TIM14.delay(&mut rcc);
 
-    let  timeout = 5000;
+    adc.set_sample_time(SampleTime::T_80);
+    adc.set_precision(Precision::B_12);
+    adc.set_oversampling_ratio(OversamplingRatio::X_16);
+    adc.set_oversampling_shift(16);
+    adc.oversampling_enable(true);
+
+    delay.delay(20.us()); // Wait for ADC voltage regulator to stabilize
+    adc.calibrate();
+
+    let timeout = 5000;
     let mut timer = dp.TIM16.timer(&mut rcc);
     let gpioa = dp.GPIOA.split(&mut rcc);
+    let gpiob = dp.GPIOB.split(&mut rcc);
     //let gpioc = dp.GPIOC.split(&mut rcc);
     let mut led = gpioa.pa11.into_push_pull_output();
     let pir = gpioa.pa12.into_pull_down_input();
+    let mut opto_pin = gpiob.pb8.into_analog();
     //pir.listen(SignalEdge.Rising, );
     let mut preview_pir_status = false;
     let mut timer_need_start = false;
@@ -32,13 +45,11 @@ fn main() -> ! {
 
     //let mut opto_pin = gpioc.pc14.into_analog();
     loop {
-        let pir_status = match pir.is_high() {
-            Ok(true) => true,
-            Ok(false) => false,
-            _ => unreachable!(),
-        };
-        if pir_status {
+        let pir_status = pir.is_high().unwrap();
+        let opto_data = adc.read_voltage(&mut opto_pin).unwrap();
 
+        rprintln!("opto read data {}", opto_data);
+        if pir_status {
             if preview_pir_status == false {
 
                 rprintln!("pir up");
