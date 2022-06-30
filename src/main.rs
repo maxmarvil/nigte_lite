1f70ccf41d541d9cc49d8264ed5337aaed920a72
marvil <marvil08@gmail.com>
2022-06-30 08:52:28 +0300

    deep sleep

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,10 @@ mod app {
               analog::adc::{ OversamplingRatio, Precision, SampleTime, Adc, }};
     //use rtt_target::{rprintln, rtt_init_print};
     use stm32g0xx_hal::gpio::Analog;
-    use systick_monotonic::{ fugit::Duration, Systick};//
+    use systick_monotonic::{ Systick};//fugit::Duration,fugit::Hertz,
     use hal::power::{LowPowerMode, PowerMode};
     use cortex_m::peripheral::{SCB};
+    use hal::timer::Timer;
 
 
     #[shared]
@@ -30,10 +31,10 @@ mod app {
         pwm_max: u32,
         delay: Delay<TIM14>,
         led_status: bool,
-        fade_out_handle: Option<fade_out::SpawnHandle>,
         fade_timer: bool,
         adc: Adc,
         opto_pin: PB7<Analog>,//Channel<Adc>,
+        timer: Timer<stm32::TIM17>,
     }
 
     #[local]
@@ -54,13 +55,15 @@ mod app {
         let gpioa = ctx.device.GPIOA.split(&mut rcc);
         let gpiob = ctx.device.GPIOB.split(&mut rcc);
         let pir = gpioa.pa12.into_pull_down_input();
-        let mut tim_ower = ctx.device.TIM17.timer(&mut rcc);
         let pwm = ctx.device.TIM3.pwm(1.kHz(), &mut rcc);
         //let trigger = None;
         let mut pwm_ch2 = pwm.bind_pin(gpiob.pb0);
         let mut delay = ctx.device.TIM14.delay(&mut rcc);
         //rtt_init_print!();
         let mut adc = ctx.device.ADC.constrain(&mut rcc);
+        let timer = ctx.device.TIM17.timer(&mut rcc);
+        //timer.start(1.kHz<1,100>().into_duration());
+        //timer.listen();
 
         let mut power = ctx.device.PWR.constrain(&mut rcc);
         power.set_mode(PowerMode::UltraLowPower(LowPowerMode::StopMode2));
@@ -75,7 +78,6 @@ mod app {
         adc.calibrate();
 
 
-        tim_ower.listen();
         //rprintln!("start");
         let mut exti = ctx.device.EXTI;
         pwm_ch2.enable();
@@ -93,10 +95,10 @@ mod app {
                 pwm_max,
                 delay,
                 led_status: false,
-                fade_out_handle: None,
                 fade_timer: false,
                 adc,
                 opto_pin,
+                timer
             },
             Local {
                 exti,
@@ -106,21 +108,23 @@ mod app {
         )
     }
 
-    #[task(binds = EXTI4_15, priority = 3, local = [exti], shared=[pir_status, fade_out_handle, fade_timer, adc, opto_pin, led_status])]
+    #[task(binds = TIM17, shared = [ ])]
+    fn timer_tick(_ctx: timer_tick::Context) {
+        fade_out::spawn().unwrap();
+    }
+
+    #[task(binds = EXTI4_15, priority = 3, local = [exti], shared=[pir_status, adc, opto_pin, led_status,timer])]
     fn pir_signal(ctx: pir_signal::Context) {
         let mut pir_status = ctx.shared.pir_status;
-        //let mut led_status = ctx.shared.led_status;
-        let mut fade_out_handle = ctx.shared.fade_out_handle;
-        let mut fade_timer = ctx.shared.fade_timer;
         let opto_pin = ctx.shared.opto_pin;
         let adc = ctx.shared.adc;
-        let fade_timer_status = fade_timer.lock(|fade_timer| *fade_timer);
-        //let status_led  = led_status.lock(|led_status| *led_status);
         let exti = ctx.local.exti;
         use SignalEdge::*;
+        let mut timer = ctx.shared.timer;
+
+        timer.lock(|timer| {timer.listen();});
 
         let status_up = exti.is_pending(exti::Event::GPIO12, Rising);
-        //let status_fall = exti.is_pending(exti::Event::GPIO12, Falling);
 
         pir_status.lock(|pir_status|{
             *pir_status = status_up;
@@ -136,41 +140,16 @@ mod app {
 
         //rprintln!("status led-{} opto:{}", status_led, opto_data);
         if status_up  {
-            if opto_data<200 {
+            if opto_data < 200 {
                 fade_in::spawn().unwrap();
-                if fade_timer_status {
-                    fade_out_handle.lock(|fade_out_h| {
-                        let taked = fade_out_h.take();
-                        //rprintln!("tacked {:?}", taked);
-                        if let Some(handler_task) = taked{
-                            //handler_task.cancel();
-                            let resp = handler_task.cancel();
-                            match resp {
-                                Ok(_) => cortex_m::asm::nop(),//rprintln!("handler result {:?}", respon),
-                                Err(_) => cortex_m::asm::nop(),//rprintln!("cancel false"),
-                            };
-                        }
-                    });
-                }
-                fade_timer.lock(|fade_timer| { *fade_timer = false; } );
-            }
-
-
-        } else { //if status_fall && status_led && fade_timer_status == false
-
-            let handler_new;
-            //fade_out::spawn().unwrap();
-            if let Ok(handler) = fade_out::spawn_after(Duration::<u64, 1, 100>::from_ticks(500)) {//
-                handler_new = Some(handler);
-                (fade_timer).lock(|fade_timer| {
-                    *fade_timer = true;
+                (timer).lock(|timer| {
+                    timer.reset();
                 });
-            } else {
-                handler_new = None;
             }
 
-            (fade_out_handle).lock(|fade_out_handle| {
-                *fade_out_handle = handler_new;
+        } else  {
+            (timer).lock(|tim_ower| {
+                tim_ower.start(6000.millis());
             });
         }
 
@@ -203,16 +182,16 @@ mod app {
         }
     }
 
-    #[task(priority = 2, local=[scb], shared=[led, pwm_max, delay, led_status,fade_out_handle])]
+    #[task(priority = 2, local=[scb], shared=[led, pwm_max, delay, led_status, timer])]
     fn fade_out(ctx: fade_out::Context) {
         let mut led = ctx.shared.led;
         let mut led_status = ctx.shared.led_status;
         let mut delay = ctx.shared.delay;
         let mut pwm_max = ctx.shared.pwm_max;
+        let mut timer = ctx.shared.timer;
         let scb = ctx.local.scb;
         let max  = pwm_max.lock(|pwm_max| *pwm_max);
         let status_led  = led_status.lock(|led_status| *led_status);
-        let mut fade_out_handle = ctx.shared.fade_out_handle;
 
         if status_led {
             //rprintln!("led off");
@@ -220,27 +199,27 @@ mod app {
                 *led_status = false;
             });
             for val in 0..100 {
-                delay.lock(|delay| delay.delay(10.millis()));
+                delay.lock(|delay| delay.delay(5.millis()));
                 let re_pr = 100 - val;
                 (led).lock(|led| {
                     led.set_duty(max * re_pr / 100);
                 });
             }
-            fade_out_handle.lock(|handler|{
-                *handler = None;
+            (timer).lock(|timer| {
+                timer.clear_irq();
             });
             (led).lock(|led| {
                 led.set_duty(0);
             });
             delay.lock(|delay| delay.delay(50.millis()));
             scb.set_sleepdeep();
+            scb.set_sleeponexit();
         }
     }
 
     #[idle]
     fn idle(_: idle::Context) -> ! {
         loop {
-            //cortex_m::asm::nop();
             cortex_m::asm::wfi();
         }
     }
