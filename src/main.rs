96142d5acbf0e4d06af7b2c8ce1ad6b5054af584
marvil <marvil08@gmail.com>
2022-07-10 20:35:40 +0300

    interrup pir

--- src/main.rs
+++ src/main.rs
@@ -2,171 +2,76 @@
 #![no_main]
 //#![deny(warnings)]
 
-extern crate panic_semihosting;
+extern crate panic_probe;
 
+use core::{cell::RefCell, ops::DerefMut};
+use cortex_m::{asm, interrupt::Mutex, peripheral::NVIC};
 use cortex_m_rt::entry;
 use rtt_target::{rprintln, rtt_init_print};
-use stm32g0xx_hal::{
-    analog::adc::{OversamplingRatio, Precision, SampleTime},
-    exti::Event,
+use stm32l0xx_hal::{
+    //adc::{OversamplingRatio, Precision, SampleTime},
+    exti::{Exti, ExtiLine, GpioLine, TriggerEdge},
     gpio::{
-        gpioa::{self, PA12},
-        gpiob::{self, PB0},
-        Output, PushPull, SignalEdge,
+        gpioa::{PA4, PA9},
+        Input, Output, PullDown, PushPull,
     },
-    pac::{self, Peripherals, EXTI},
+    pac::{Interrupt, Peripherals},
     prelude::*,
-    rcc::{Config, Prescaler},
-    stm32::{self, interrupt, Interrupt, TIM14, TIM3},
-    timer::{delay::Delay, pwm::PwmPin, Channel3},
+    rcc::Config,
+    syscfg::SYSCFG,
 };
-//use stm32g0xx_hal::rcc::SysClockSrc;
-use core::{cell::RefCell, ops::DerefMut};
-use cortex_m::{interrupt::Mutex, peripheral::Peripherals as c_m_Peripherals};
 
-static LED: Mutex<RefCell<Option<gpiob::PB0<Output<PushPull>>>>> = Mutex::new(RefCell::new(None));
-static INT: Mutex<RefCell<Option<EXTI>>> = Mutex::new(RefCell::new(None));
+static LED: Mutex<RefCell<Option<PA4<Output<PushPull>>>>> = Mutex::new(RefCell::new(None));
+static PIR: Mutex<RefCell<Option<PA9<Input<PullDown>>>>> = Mutex::new(RefCell::new(None));
+
+#[interrupt]
+fn EXTI4_15() {
+    rprintln!("EXTI interrupt");
+
+    cortex_m::interrupt::free(|cs| {
+        Exti::unpend(GpioLine::from_raw_line(9).unwrap());
+
+        if let (&mut Some(ref mut led), &mut Some(ref mut pir)) = (
+            LED.borrow(cs).borrow_mut().deref_mut(),
+            PIR.borrow(cs).borrow_mut().deref_mut(),
+        ) {
+            if pir.is_low().is_ok() {
+                led.set_low().unwrap();
+            } else {
+                led.set_high().unwrap();
+            }
+        }
+    });
+}
+
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     rprintln!("start");
     let cp = cortex_m::Peripherals::take().expect("cannot take core peripherals");
-    let dp = stm32::Peripherals::take().expect("cannot take peripherals");
-    let mut rcc = dp.RCC.freeze(Config::hsi(Prescaler::NotDivided)); //Config::new(SysClockSrc::PLL)
-                                                                     //let mut adc = dp.ADC.constrain(&mut rcc);
-    let mut delay = dp.TIM14.delay(&mut rcc);
-
-    // adc.set_sample_time(SampleTime::T_80);
-    // adc.set_precision(Precision::B_12);
-    // adc.set_oversampling_ratio(OversamplingRatio::X_16);
-    // adc.set_oversampling_shift(16);
-    // adc.oversampling_enable(true);
-
-    delay.delay(20.micros()); // Wait for ADC voltage regulator to stabilize
-                              //adc.calibrate();
-
-    let timeout = 5000;
-    //let mut timer = dp.TIM16.timer(&mut rcc);
+    let dp = Peripherals::take().unwrap();
+    let mut rcc = dp.RCC.freeze(Config::hsi16());
     let gpioa = dp.GPIOA.split(&mut rcc);
-    let gpiob = dp.GPIOB.split(&mut rcc);
-    let mut pwm_pin = gpiob.pb0.into_push_pull_output();
-    //pwm_pin.enable();
-    let mut exti = dp.EXTI;
-    let syscfg = dp.SYSCFG;
+    let mut pwm_pin = gpioa.pa4.into_push_pull_output();
 
-    //let mut pwm_pin = pwm.bind_pin(gpiob.pb0);
-    pwm_pin.set_low();
-    let mut pir = gpioa.pa12.into_pull_down_input();
-    // let pwm = dp.TIM3.pwm(1.kHz(), &mut rcc);
+    let mut syscfg = SYSCFG::new(dp.SYSCFG, &mut rcc);
+    let mut exti = Exti::new(dp.EXTI);
 
-    // let mut opto_pin = gpiob.pb7.into_analog(); // opto-transistor temt6000
-    // let mut preview_pir_status = false;
-    // let mut timer_need_start = false;
-    // let mut timer_started = false;
-    // let mut led_on_status = false;
-    //pir.listen(SignalEdge::Rising, &mut exti);
-    exti.exticr4.modify(|r, w| unsafe { w.exti8_15().bits(1) });
-    exti.imr1.modify(|_, w| w.im12().set_bit());
-    exti.rtsr1.modify(|_, w| w.tr12().set_bit());
-    exti.ftsr1.modify(|_, w| w.tr12().set_bit());
+    let mut pir = gpioa.pa9.into_pull_down_input();
+
+    let line = GpioLine::from_raw_line(pir.pin_number()).unwrap();
+    exti.listen_gpio(&mut syscfg, pir.port(), line, TriggerEdge::Both);
 
     cortex_m::interrupt::free(move |cs| {
-        //syscfg.cfgr2exticr2.modify(|_, w| unsafe { w.exti4().bits(1) });
-
         *LED.borrow(cs).borrow_mut() = Some(pwm_pin);
-        *INT.borrow(cs).borrow_mut() = Some(exti);
+        *PIR.borrow(cs).borrow_mut() = Some(pir);
     });
-    //let max = pwm_pin.get_max_duty();
-    //rprintln!("pwm {}", max);
-    //let mut nvic = cp.NVIC;
-    //unsafe {
-    //nvic.set_priority(Interrupt::EXTI4_15, 1);
-    //cortex_m::peripheral::NVIC::unmask(Interrupt::EXTI4_15);
-    //}
+
+    unsafe {
+        NVIC::unmask(Interrupt::EXTI4_15);
+    }
 
     loop {
-        continue;
-
-        // let pir_status = pir.is_high().unwrap();
-        // let opto_data = adc.read_voltage(&mut opto_pin).unwrap();
-        //
-        // //rprintln!("opto data {}", opto_data);
-        //
-        // if opto_data > 20 && led_on_status == false {
-        //     preview_pir_status = false;
-        //     timer_need_start = false;
-        //     continue;
-        // }
-        //
-        // if pir_status {
-        //     if preview_pir_status == false {
-        //         rprintln!("pir up");
-        //         led_on(&mut delay, &mut pwm_pin);
-        //         led_on_status = true;
-        //         preview_pir_status = true;
-        //         timer_need_start = true;
-        //     }
-        //     if timer_started {
-        //         timer.reset();
-        //     }
-        // } else if preview_pir_status {
-        //     if timer_need_start {
-        //         timer_started = true;
-        //         timer.start(timeout.millis());
-        //     }
-        //     timer_need_start = false;
-        //
-        //     if timer.wait().is_ok() {
-        //         // новый цикл таймера
-        //         rprintln!("timer is ok");
-        //         led_off(&mut delay, &mut pwm_pin);
-        //         led_on_status = false;
-        //         timer.reset();
-        //         timer.clear_irq();
-        //         preview_pir_status = false;
-        //     }
-        // }
+        asm::wfi();
     }
-}
-//
-// fn led_on(delay: &mut Delay<TIM14>, pin: &mut PwmPin<TIM3, Channel3>) {
-//     let max = pin.get_max_duty();
-//     rprintln!("led_on");
-//     for pr in 0..100 {
-//         delay.delay(10.millis());
-//         pin.set_duty(max * pr / 100);
-//     }
-//     pin.set_duty(max);
-// }
-//
-// fn led_off(delay: &mut Delay<TIM14>, pin: &mut PwmPin<TIM3, Channel3>) {
-//     let max = pin.get_max_duty();
-//     rprintln!("led_off");
-//     for pr in 0..100 {
-//         let re_pr = 100 - pr;
-//         delay.delay(10.millis());
-//         pin.set_duty(max * re_pr / 100);
-//     }
-//     pin.set_duty(0);
-// }
-
-#[interrupt]
-fn EXTI4_15() {
-    rprintln!("EXTI interrupt");
-    cortex_m::interrupt::free(|cs| {
-        // Obtain all Mutex protected resources
-        if let (&mut Some(ref mut led), &mut Some(ref mut exti)) = (
-            LED.borrow(cs).borrow_mut().deref_mut(),
-            INT.borrow(cs).borrow_mut().deref_mut(),
-        ) {
-            // Turn on LED
-            led.toggle().ok();
-
-            // Turn off LED
-            rprintln!("EXTI take");
-
-            // Clear event triggering the interrupt
-            exti.unpend(Event::GPIO13);
-        }
-    });
 }
