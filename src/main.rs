481e7664a6712a20dea0d6ad140a54ef64c2403f
marvil <marvil08@gmail.com>
2022-07-17 13:00:06 +0300

    wip +pwm + configuration optotransistor

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,18 @@
 
 extern crate panic_probe;
 
-use core::borrow::BorrowMut;
 use core::{cell::RefCell, ops::DerefMut};
-use cortex_m::peripheral::SCB;
-use cortex_m::{asm, interrupt::Mutex, peripheral::NVIC};
+use cortex_m::{interrupt::Mutex, peripheral::NVIC};
 use cortex_m_rt::entry;
-use rtt_target::{rprintln, rtt_init_print};
-use stm32l0::stm32l0x1::{interrupt, TIM2, TIM22};
-use stm32l0xx_hal::rcc::Rcc;
+use stm32l0::stm32l0x1::{interrupt, TIM2};
 use stm32l0xx_hal::{
     adc::{Adc, Ready},
-    exti::{
-        self, Exti, ExtiLine, GpioLine,
-        TriggerEdge::{self, Falling, Rising},
-    },
+    exti::{Exti, ExtiLine, GpioLine, TriggerEdge},
     gpio::{
-        gpioa::{PA0, PA1, PA3, PA9},
-        Analog, Input, Output, PullDown, PushPull,
+        gpioa::{PA0, PA1, PA3},
+        Analog,
     },
-    pac::{self, Interrupt, Peripherals, EXTI},
+    pac::{self, Interrupt},
     prelude::*,
     pwm::{self, Assigned, Pwm, C4},
     pwr::{self, PWR},
@@ -33,143 +26,177 @@ use stm32l0xx_hal::{
 
 static LED: Mutex<RefCell<Option<Pwm<TIM2, C4, Assigned<PA3<Analog>>>>>> =
     Mutex::new(RefCell::new(None));
-static TIMER: Mutex<RefCell<Option<Timer<pac::TIM22>>>> = Mutex::new(RefCell::new(None));
+static LED_STATUS: Mutex<RefCell<Option<bool>>> = Mutex::new(RefCell::new(Some(false)));
 static COUNT_D: Mutex<RefCell<Option<u8>>> = Mutex::new(RefCell::new(None));
+static TIMER: Mutex<RefCell<Option<Timer<pac::TIM22>>>> = Mutex::new(RefCell::new(None));
 static ADC: Mutex<RefCell<Option<Adc<Ready>>>> = Mutex::new(RefCell::new(None));
 static OPTOPIN: Mutex<RefCell<Option<PA1<Analog>>>> = Mutex::new(RefCell::new(None));
+static SENSPIN: Mutex<RefCell<Option<PA0<Analog>>>> = Mutex::new(RefCell::new(None));
 static SHIFT: Mutex<RefCell<Option<u16>>> = Mutex::new(RefCell::new(None));
-static LED_STATUS: Mutex<RefCell<Option<bool>>> = Mutex::new(RefCell::new(Some(false)));
 
 static TIMEOUT: u8 = 4;
 
 #[entry]
 fn main() -> ! {
-    rtt_init_print!();
-    rprintln!("start");
-    let cp = cortex_m::Peripherals::take().expect("cannot take core peripherals");
-    let dp = Peripherals::take().unwrap();
+    let cp = pac::CorePeripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+
     let mut rcc = dp.RCC.freeze(Config::hsi16());
     let gpioa = dp.GPIOA.split(&mut rcc);
+    let mut exti = Exti::new(dp.EXTI);
     let mut pwr = PWR::new(dp.PWR, &mut rcc);
+    let mut delay = cp.SYST.delay(rcc.clocks);
+    let timer = dp.TIM22.timer(1.Hz(), &mut rcc);
+    let adc = dp.ADC.constrain(&mut rcc);
+    let opto_pin = gpioa.pa1.into_analog();
+    let sens_pin = gpioa.pa0.into_analog();
     let mut scb = cp.SCB;
     let mut light_status = false;
 
-    let mut syscfg = SYSCFG::new(dp.SYSCFG, &mut rcc);
-    let mut exti = Exti::new(dp.EXTI);
-    let mut timer = dp.TIM22.timer(1.Hz(), &mut rcc);
-    let mut adc = dp.ADC.constrain(&mut rcc);
-    let mut opto_pin = gpioa.pa1.into_analog();
-    let mut sens_pin = gpioa.pa0.into_analog();
+    // Those are the user button and blue LED on the B-L072Z-LRWAN1 Discovery
+    // board.
+    let trigger_up = gpioa.pa9.into_floating_input();
+    let mut led_red = gpioa.pa5.into_push_pull_output();
+    let pir2 = gpioa.pa10.into_pull_down_input();
     let pwm = pwm::Timer::new(dp.TIM2, 1_000.Hz(), &mut rcc);
-    let mut pwm_pin: Pwm<TIM2, C4, Assigned<PA3<Analog>>> = pwm.channel4.assign(gpioa.pa3);
-    pwm_pin.enable();
+    let mut led: Pwm<TIM2, C4, Assigned<PA3<Analog>>> = pwm.channel4.assign(gpioa.pa3);
+    led.enable();
 
-    let correcting: u16 = adc.read(&mut sens_pin).unwrap();
+    let mut syscfg = SYSCFG::new(dp.SYSCFG, &mut rcc);
 
-    let mut pir = gpioa.pa9.into_pull_down_input();
-    let mut pir2 = gpioa.pa10.into_pull_down_input();
-
-    let line = GpioLine::from_raw_line(pir.pin_number()).unwrap();
+    let line = GpioLine::from_raw_line(trigger_up.pin_number()).unwrap();
     let line2 = GpioLine::from_raw_line(pir2.pin_number()).unwrap();
-    //let exti_line = GpioLine::from_raw_line(gpioa.pa8.into_pull_down_input().pin_number()).unwrap();
 
-    exti.listen_gpio(&mut syscfg, pir.port(), line, Rising);
-    exti.listen_gpio(&mut syscfg, pir2.port(), line2, Falling);
+    exti.listen_gpio(&mut syscfg, trigger_up.port(), line, TriggerEdge::Rising);
 
-    cortex_m::interrupt::free(move |cs| {
-        //*LED.borrow(cs).borrow_mut() = Some(pwm_pin);
+    cortex_m::interrupt::free(|cs| {
+        *LED.borrow(cs).borrow_mut() = Some(led);
         *TIMER.borrow(cs).borrow_mut() = Some(timer);
         *COUNT_D.borrow(cs).borrow_mut() = Some(0);
         *ADC.borrow(cs).borrow_mut() = Some(adc);
         *OPTOPIN.borrow(cs).borrow_mut() = Some(opto_pin);
-        *SHIFT.borrow(cs).borrow_mut() = Some(correcting);
+        *SENSPIN.borrow(cs).borrow_mut() = Some(sens_pin);
     });
 
-    unsafe {
-        NVIC::unmask(Interrupt::EXTI4_15);
-        NVIC::unmask(Interrupt::TIM22);
-    }
-    for _ in 0..5_000_000 {
-        asm::nop()
-    }
-    //exti.wait_for_irq(exti_line, pwr.low_power_sleep_mode(&mut scb, &mut rcc));
-    loop {
-        cortex_m::interrupt::free(|cs| {
-            if let &mut Some(ref mut led_status) = LED_STATUS.borrow(cs).borrow_mut().deref_mut() {
-                if *led_status && light_status == false {
-                    light_status = true;
-                    let max_value = pwm_pin.get_max_duty();
-                    for pr in 0..100 {
-                        delay();
-                        let duty = ((max_value as f32) * (pr as f32) / 100.0);
-                        pwm_pin.set_duty(duty as u16);
-                        //rprintln!("on {}", (pr as f32)); //((max_value as f32) * (pr as f32) / 100.0)
-                    }
+    delay.delay_ms(5000u32);
 
-                    pwm_pin.set_duty(max_value);
-                } else if *led_status == false && light_status == true {
-                    light_status = false;
-                    let max_value = pwm_pin.get_max_duty();
+    loop {
+        if light_status == false {
+            delay.delay_ms(100u32);
+            led_red.set_high().unwrap();
+            delay.delay_ms(100u32);
+            led_red.set_low().unwrap();
+
+            exti.wait_for_irq(
+                line,
+                pwr.stop_mode(
+                    &mut scb,
+                    &mut rcc,
+                    pwr::StopModeConfig {
+                        ultra_low_power: true,
+                    },
+                ),
+            );
+            cortex_m::interrupt::free(|cs| {
+                if let (&mut Some(ref mut adc), &mut Some(ref mut sens_pin)) = (
+                    ADC.borrow(cs).borrow_mut().deref_mut(),
+                    SENSPIN.borrow(cs).borrow_mut().deref_mut(),
+                ) {
+                    let correcting: u16 = adc.read(sens_pin).unwrap();
+                    *SHIFT.borrow(cs).borrow_mut() = Some(correcting);
+                }
+            });
+
+            exti.listen_gpio(&mut syscfg, pir2.port(), line2, TriggerEdge::Falling);
+            unsafe {
+                NVIC::unmask(Interrupt::EXTI4_15);
+                NVIC::unmask(Interrupt::TIM22);
+            }
+
+            delay.delay_ms(1u32);
+
+            cortex_m::interrupt::free(|cs| {
+                if let (
+                    &mut Some(ref mut led),
+                    &mut Some(ref mut led_status),
+                    &mut Some(ref mut adc),
+                    &mut Some(ref mut opto_pin),
+                    &mut Some(ref mut correcting),
+                ) = (
+                    LED.borrow(cs).borrow_mut().deref_mut(),
+                    LED_STATUS.borrow(cs).borrow_mut().deref_mut(),
+                    ADC.borrow(cs).borrow_mut().deref_mut(),
+                    OPTOPIN.borrow(cs).borrow_mut().deref_mut(),
+                    SHIFT.borrow(cs).borrow_mut().deref_mut(),
+                ) {
+                    let val: u16 = adc.read(opto_pin).unwrap();
+                    let index: f32 = (*correcting as f32) / 4095.0;
+                    let val_new = (val as f32) * index;
+
+                    if val_new < 100.0 {
+                        let max_value = led.get_max_duty();
+                        for pr in 0..100 {
+                            delay.delay_ms(10u32);
+                            let duty = (max_value as f32) * (pr as f32) / 100.0;
+                            led.set_duty(duty as u16);
+                            //rprintln!("on {}", (pr as f32)); //((max_value as f32) * (pr as f32) / 100.0)
+                        }
+                        led.set_duty(max_value);
+
+                        light_status = true;
+                        *led_status = true;
+                    }
+                }
+            });
+        }
+
+        delay.delay_ms(5u32);
+
+        cortex_m::interrupt::free(|cs| {
+            if let (&mut Some(ref mut led), &mut Some(ref mut led_status)) = (
+                LED.borrow(cs).borrow_mut().deref_mut(),
+                LED_STATUS.borrow(cs).borrow_mut().deref_mut(),
+            ) {
+                if *led_status == false && light_status == true {
+                    //led.set_low().unwrap();
+                    let max_value = led.get_max_duty();
                     for pr in 0..100 {
                         let re_pr = 100 - pr;
-                        //rprintln!("off {}", ((max_value as f32) * ((re_pr as f32) / 100.0)));
-                        delay();
-                        let duty = ((max_value as f32) * (re_pr as f32) / 100.0);
-                        pwm_pin.set_duty(duty as u16);
+                        delay.delay_ms(10u32);
+                        let duty = (max_value as f32) * (re_pr as f32) / 100.0;
+                        led.set_duty(duty as u16);
                     }
-                    pwm_pin.set_duty(0);
+                    led.set_duty(0);
+                    light_status = false;
                 }
             }
         });
-        asm::wfi();
     }
 }
 
 #[interrupt]
 fn EXTI4_15() {
     cortex_m::interrupt::free(|cs| {
-        if let (
-            //&mut Some(ref mut led),
-            &mut Some(ref mut timer),
-            &mut Some(ref mut adc),
-            &mut Some(ref mut opto_pin),
-            &mut Some(ref mut correcting),
-            &mut Some(ref mut led_status),
-        ) = (
-            //LED.borrow(cs).borrow_mut().deref_mut(),
+        if let (&mut Some(ref mut timer), &mut Some(ref mut led_status)) = (
             TIMER.borrow(cs).borrow_mut().deref_mut(),
-            ADC.borrow(cs).borrow_mut().deref_mut(),
-            OPTOPIN.borrow(cs).borrow_mut().deref_mut(),
-            SHIFT.borrow(cs).borrow_mut().deref_mut(),
             LED_STATUS.borrow(cs).borrow_mut().deref_mut(),
-            //SENSPIN.borrow(cs).borrow_mut().deref_mut(),
         ) {
             let event_risen = Exti::is_pending(GpioLine::from_raw_line(9).unwrap());
             let event_falling = Exti::is_pending(GpioLine::from_raw_line(10).unwrap());
-            let mut val: u16 = adc.read(opto_pin).unwrap();
-            let mut index: f32 = (*correcting as f32) / 4095.0;
-            let mut val_new = (val as f32) * index;
-            rprintln!("opto {} ", (val_new as u32));
-            if event_risen && *led_status == false {
-                // && val < 200
-                rprintln!("EXTI high");
-                if val_new < 100.0 {
-                    *led_status = true;
-                }
-            } else if event_risen && *led_status {
-                rprintln!("EXTI high refrash");
+
+            if event_risen && *led_status {
+                *led_status = true;
                 timer.unlisten();
                 *COUNT_D.borrow(cs).borrow_mut() = Some(0);
             }
-            if event_falling && *led_status == true {
-                rprintln!("EXTI low");
+
+            if event_falling && *led_status {
                 timer.reset();
                 timer.listen();
             }
             Exti::unpend(GpioLine::from_raw_line(9).unwrap());
             Exti::unpend(GpioLine::from_raw_line(10).unwrap());
         }
-        //LED_STATUS.borrow(cs).replace(Some(led_status));
     });
 }
 
@@ -181,11 +208,9 @@ fn TIM22() {
             COUNT_D.borrow(cs).borrow_mut().deref_mut(),
             LED_STATUS.borrow(cs).borrow_mut().deref_mut(),
         ) {
-            // Clear the interrupt flag.
             timer.clear_irq();
 
             *count = *count + 1;
-            rprintln!("Timer catch {}", count);
             if *count > TIMEOUT && *led_status == true {
                 timer.unlisten();
                 *count = 0;
@@ -193,10 +218,4 @@ fn TIM22() {
             }
         }
     });
-}
-
-fn delay() {
-    for _ in 0..10000 {
-        asm::nop()
-    }
 }
